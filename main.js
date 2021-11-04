394200a930cb30a7f8c2f0c72e223d805f05fac8
Jay Park <jayhpark99@gmail.com>
2021-11-04 14:54:06 -0700

    completed issue-3

--- main.js
+++ main.js
@@ -4,17 +4,24 @@ var data = {
 var $car = document.querySelector('img');
 document.addEventListener('keydown', changeDirection);
 function changeDirection(event) {
-  if (`${event.code}` === 'ArrowUp') {
+  if (event.key === 'ArrowUp') {
     data.direction = 'north';
     $car.className = data.direction;
-  } else if (`${event.code}` === 'ArrowRight') {
+  } else if (event.key === 'ArrowRight') {
     data.direction = 'east';
     $car.className = data.direction;
-  } else if (`${event.code}` === 'ArrowLeft') {
+  } else if (event.key === 'ArrowLeft') {
     data.direction = 'west';
     $car.className = data.direction;
-  } else if (`${event.code}` === 'ArrowDown') {
+  } else if (event.key === 'ArrowDown') {
     data.direction = 'south';
     $car.className = data.direction;
   }
+}
+
+var x = 0;
+var interval = setInterval(startCar, 16);
+function startCar() {
+  $car.style.left = x++ + 'px';
+  location[0] = x;
 }
